990f6f293d16894606f1edd99ca7ac3e3dc803d2
alief572 <146901652+alief572@users.noreply.github.com>
2025-06-04 15:14:21 +0700

    Perbaikan - Master Budget Pembayaran Periodik
    Status setelah di save error, tapi ada tetap mask

--- application/modules/budget_coa/controllers/Budget_coa.php
+++ application/modules/budget_coa/controllers/Budget_coa.php
@@ -191,8 +191,8 @@ class Budget_coa extends Admin_Controller
 			$result			= TRUE;
 		} else {
 			for ($x = 0; $x < count($coa); $x++) {
-				if ($finance_tahun[$x] == '') $finance_tahun[$x] = 0;
-				if ($finance_bulan[$x] == '') $finance_bulan[$x] = 0;
+				if (isset($finance_tahun[$x]) && $finance_tahun[$x] == '') $finance_tahun[$x] = 0;
+				if (isset($finance_bulan[$x]) && $finance_bulan[$x] == '') $finance_bulan[$x] = 0;
 				//			  if($finance_tahun[$x]>0){
 				$data =  array(
 					'tahun' => $tahun,
