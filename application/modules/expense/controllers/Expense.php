b442cff5db3d1c428832acef1cc876a8dd2db1f4
alief572 <146901652+alief572@users.noreply.github.com>
2025-06-25 22:18:01 +0700

    Perbaikan - Transport

--- application/modules/expense/controllers/Expense.php
+++ application/modules/expense/controllers/Expense.php
@@ -1731,7 +1731,7 @@ class Expense extends Admin_Controller
 		$this->db->trans_begin();
 		$config['upload_path'] = 'assets/expense/';
 		$config['allowed_types'] = '*';
-		// $config['max_size'] = 9999999999999999999;
+		$config['max_size'] = 9999999999999999999;
 		$config['remove_spaces'] = TRUE;
 		$config['encrypt_name'] = TRUE;
 		$filenames = $filename;
