12fab007d9175d595313fcfec4fa7d0a4034cb15
alief572 <146901652+alief572@users.noreply.github.com>
2025-06-30 21:45:46 +0700

    Perbaikan - Approval Budget Pembelian Asset
    Buat tidak bisa approve jika tidak pilih antara approve atau reject budget

--- application/modules/asset_planning/views/add.php
+++ application/modules/asset_planning/views/add.php
@@ -275,6 +275,21 @@ $rev_keterangan 	= (!empty($header)) ? strtoupper($header[0]->rev_keterangan) : 
 		e.preventDefault();
 		$('#save').prop('disabled', true);
 
+		var approve = "<?= $approve ?>";
+		if (approve !== '') {
+			var status = $('#status').val();
+
+			if (status == '') {
+				swal({
+					type: 'warning',
+					title: 'Warning !',
+					text: 'Approve status must be choosen !'
+				});
+
+				return false;
+			}
+		}
+
 		var department = $('#department').val();
 
 		if (department == '0') {
