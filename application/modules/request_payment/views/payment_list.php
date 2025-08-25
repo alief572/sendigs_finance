d1da6cf392a090cc7e27f580eb1be6742f688b32
alief572 <146901652+alief572@users.noreply.github.com>
2025-08-25 15:18:16 +0700

    Perbaikan - Payment List
    1. Status hanya ada Paid dan Open saja
    2. jika kasbon based on projet, maka ID kasbon dari program consultant yang dimunculkan

--- application/modules/request_payment/views/payment_list.php
+++ application/modules/request_payment/views/payment_list.php
@@ -16,7 +16,7 @@ $ENABLE_VIEW    = has_permission('Payment_List.View');
 		<!-- <div class="col-md-6"> -->
 		<!-- <div class="form-inline"> -->
 		<div class="row">
-			<div class="col-md-3">
+			<!-- <div class="col-md-3">
 				<div class="form-group">
 					<input type="date" name="tgl_from" id="" class="form-control form-control-sm tgl_from">
 				</div>
@@ -40,15 +40,15 @@ $ENABLE_VIEW    = has_permission('Payment_List.View');
 						?>
 					</select>
 				</div>
-			</div>
+			</div> -->
 			<div class="col-md-2">
-				<button type="button" class="btn btn-sm btn-primary search_data"><i class="fa fa-search"></i> Search</button>
+				<!-- <button type="button" class="btn btn-sm btn-primary search_data"><i class="fa fa-search"></i> Search</button> -->
 				<button type="button" class="btn btn-sm btn-success excel_data"><i class="fa fa-download"></i> Excel</button>
 			</div>
 		</div>
 		<!-- </div> -->
 		<!-- </div> -->
-		<div class="table-responsive col-md-12 table_container">
+		<div class="col-md-12 table_container">
 			<table id="mytabledata" class="table table-bordered">
 				<thead>
 					<tr>
@@ -73,8 +73,14 @@ $ENABLE_VIEW    = has_permission('Payment_List.View');
 						foreach ($data as $record) {
 
 							$nmuser = $record->nama;
+							$no_doc = $record->no_doc;
 							if ($record->tipe == 'kasbon') {
 								$get_kasbon = $this->db->get_where('tr_kasbon', array('no_doc' => $record->no_doc))->row();
+
+								if ($get_kasbon->no_kasbon_consultant !== null) {
+									$no_doc = $get_kasbon->no_kasbon_consultant;
+								}
+
 								$check_detail = $this->db->get_where('tr_pr_detail_kasbon', ['id_kasbon' => $record->no_doc])->result();
 								if (count($check_detail)) {
 									if ($get_kasbon->tipe_pr == 'pr departemen') {
@@ -96,6 +102,16 @@ $ENABLE_VIEW    = has_permission('Payment_List.View');
 
 										$nmuser = $get_single_detail->nm_lengkap;
 									}
+
+									if ($get_kasbon->tipe_pr == 'pr asset') {
+										$this->db->select('b.nm_lengkap');
+										$this->db->from('tran_pr_header a');
+										$this->db->join('users b', 'b.id_user = a.created_by');
+										$this->db->where('a.no_pr', $get_kasbon->id_pr);
+										$get_single_detail = $this->db->get()->row();
+
+										$nmuser = $get_single_detail->nm_lengkap;
+									}
 								}
 							}
 
@@ -110,7 +126,7 @@ $ENABLE_VIEW    = has_permission('Payment_List.View');
 							$numb++; ?>
 							<tr>
 								<td><?= $numb; ?></td>
-								<td><?= $record->no_doc ?></td>
+								<td><?= $no_doc ?></td>
 								<td><?= $nmuser ?></td>
 								<td><?= $record->tgl_doc ?></td>
 								<td><?= $record->keperluan ?></td>
@@ -122,37 +138,12 @@ $ENABLE_VIEW    = has_permission('Payment_List.View');
 								<td class="text-center"><?= $tgl_pembayaran ?></td>
 								<td>
 									<?php
-									$get_request_payment = $this->db->get_where('request_payment', ['no_doc' => $record->no_doc])->row();
-									if (!empty($get_request_payment)) {
-										if ($record->sts_reject !== '1' && $record->sts_reject_manage !== '1') {
-											if ($get_request_payment->status == '0') {
-												echo '<div class="badge bg-yellow text-light">Process</div>';
-											}
-											if ($get_request_payment->status == '1' || $get_request_payment->status == '2') {
-												$get_payment_approve = $this->db->get_where('payment_approve', ['no_doc' => $record->no_doc])->row();
-												if (!empty($get_payment_approve) && $get_payment_approve->status == '2') {
-													echo '<div class="badge bg-green text-light">Paid</div>';
-												} else {
-													echo '<div class="badge bg-yellow text-light">Approved</div>';
-												}
-											}
-										} else {
-											if ($record->sts_reject == '1') {
-												echo '<div class="badge bg-red">Rejected by Checker</div>';
-											} else if ($record->sts_reject_manage == '1') {
-												echo '<div class="badge bg-red">Rejected by Management</div>';
-											} else {
-												echo '<div class="badge bg-blue">Open</div>';
-											}
-										}
+									$get_payment = $this->db->get_where('payment_approve', ['no_doc' => $record->no_doc])->result();
+
+									if (!empty($get_payment)) {
+										echo '<div class="badge bg-green text-light">Paid</div>';
 									} else {
-										if ($record->sts_reject == '1') {
-											echo '<div class="badge bg-red">Rejected by Checker</div>';
-										} else if ($record->sts_reject_manage == '1') {
-											echo '<div class="badge bg-red">Rejected by Management</div>';
-										} else {
-											echo '<div class="badge bg-blue">Open</div>';
-										}
+										echo '<div class="badge bg-blue">Open</div>';
 									}
 									?>
 								</td>
