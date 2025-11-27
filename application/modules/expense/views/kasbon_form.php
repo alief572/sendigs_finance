a2b2569245f4f900afc71d24e884d419e3c93b70
Alief Daffa <aliefdaffa572@gmail.com>
2025-11-27 16:00:26 +0700

    Perbaikan - Expense

--- application/modules/expense/views/kasbon_form.php
+++ application/modules/expense/views/kasbon_form.php
@@ -245,7 +245,7 @@ $metode_pembayaran = (isset($data)) ? $data->metode_pembayaran : 1;
 								echo '<div class="col-md-12">
 						<iframe src="' . base_url('assets/expense/' . $data->doc_file) . '#toolbar=0&navpanes=0" title="PDF" style="width:600px; height:500px;" frameborder="0">
 						</iframe>
-						<a href="' . base_url('assets/expense/' . $data->doc_file) . '" class="btn btn-sm btn-primary" download>Download PDF</a>
+						<a href="' . base_url('assets/expense/' . $data->doc_file) . '" class="btn btn-sm btn-primary" target="_blank">Check PDF</a>
 						<br />' . $data->no_doc . '</div>';
 							} else {
 								if (file_exists('assets/expense' . $data->doc_file)) {
@@ -257,8 +257,8 @@ $metode_pembayaran = (isset($data)) ? $data->metode_pembayaran : 1;
 							if (strpos($data->doc_file_2, 'pdf', 0) > 1) {
 								echo '<div class="col-md-12">
 						<iframe src="' . base_url('./assets/expense/' . $data->doc_file_2) . '#toolbar=0&navpanes=0" title="PDF" style="width:600px; height:500px;" frameborder="0">
-								 <a href="' . base_url('assets/expense/' . $data->doc_file_2) . '">Download PDF</a>
 						</iframe>
+						<a href="' . base_url('assets/expense/' . $data->doc_file_2) . '" class="btn btn-sm btn-primary" target="_blank">Check PDF</a>
 						<br />' . $data->no_doc . '</div>';
 							} else {
 								if (file_exists('./assets/expense' . $data->doc_file_2)) {
