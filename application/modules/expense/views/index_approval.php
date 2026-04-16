c9b6426010ddfd7eb36cda16b47ca7ea25775e23
Alief Daffa <aliefdaffa572@gmail.com>
2026-04-16 11:14:49 +0700

    Perbaikan - Expense

--- application/modules/expense/views/index_approval.php
+++ application/modules/expense/views/index_approval.php
@@ -6,7 +6,6 @@ $ENABLE_DELETE  = has_permission('Expense_Approval.Delete');
 ?>
 <div id="alert_edit" class="alert alert-success alert-dismissable" style="padding: 15px; display: none;"></div>
 <div class="box">
-	<!-- /.box-header -->
 	<div class="box-body">
 		<div class="table-responsive col-md-12">
 			<table id="mytabledata" class="table table-bordered table-striped">
@@ -22,50 +21,20 @@ $ENABLE_DELETE  = has_permission('Expense_Approval.Delete');
 						<th width="120">Action</th>
 					</tr>
 				</thead>
-				<tbody>
-					<?php
-					if (!empty($data)) {
-						$numb = 0;
-						foreach ($data as $record) {
-							$numb++; ?>
-							<tr>
-								<td><?= $numb; ?></td>
-								<td><?= $record->no_doc ?></td>
-								<td><?= $record->tgl_doc ?></td>
-								<td><?= $record->nmuser ?></td>
-								<td><?= $record->informasi ?></td>
-								<td class="text-right"><?= number_format($record->nominal) ?></td>
-								<td><?= $status[$record->status] ?></td>
-								<td>
-									<?php if ($ENABLE_VIEW) : ?>
-										<a class="btn btn-warning btn-sm view" href="javascript:void(0)" title="View" onclick="data_view('<?= $record->id ?>')"><i class="fa fa-eye"></i></a>
-										<?php endif;
-									if ($ENABLE_MANAGE) :
-										if ($record->status == 0) { ?>
-											<a class="btn btn-success btn-sm approve" href="javascript:void(0)" title="Approve" onclick="data_approve('<?= $record->id ?>')"><i class="fa fa-check-square-o"></i></a>
-									<?php }
-									endif;
-									?>
-								</td>
-							</tr>
-					<?php
-						}
-					}  ?>
-				</tbody>
+				<tbody></tbody>
 			</table>
 		</div>
 	</div>
-	<!-- /.box-body -->
 </div>
 <div id="form-data"></div>
 <script src="<?= base_url('assets/plugins/datatables/jquery.dataTables.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/datatables/dataTables.bootstrap.min.js') ?>"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script type="text/javascript">
-	var url_edit = siteurl + 'expense/edit/';
-	var url_view = siteurl + 'expense/view/';
 	var url_approval = siteurl + 'expense/approval/';
-	//Edit
+
+	DataTables();
+
 	function data_approve(id) {
 		if (id != "") {
 			$(".box").hide();
@@ -74,55 +43,60 @@ $ENABLE_DELETE  = has_permission('Expense_Approval.Delete');
 		}
 	}
 
-	function datatables() {
-		var datatables = $('#mytabledata').dataTable({
+	function DataTables() {
+		$('#mytabledata').dataTable({
 			serverSide: true,
 			processing: true,
+			stateSave: true,
 			destroy: true,
 			paging: true,
-			stateSave: true,
 			ajax: {
-				type: 'post',
-				url: siteurl + active_controller + 'get_dat_approval_expense',
+				type: 'GET',
+				url: siteurl + active_controller + 'get_expense_app_finance',
 				cache: false,
-				dataType: 'json',
-				error: function(xhr, status, error) {
-					Swal.fire({
-						icon: 'error',
-						title: 'Error !',
-						text: error,
-						showConfirmButton: false,
-						showCancelButton: false,
-						allowOutsideClick: false,
-						allowEscapeKey: false,
-						timer: 3000
-					});
+				dataType: 'json'
+			},
+			columns: [{
+					data: 'no',
+					orderable: false
 				},
-				columns: [{
-						data: 'no'
-					},
-					{
-						data: 'no_doc'
-					},
-					{
-						data: 'tgl_doc'
-					},
-					{
-						data: 'nama'
-					},
-					{
-						data: 'keterangan'
-					},
-					{
-						data: 'nominal'
-					},
-					{
-						data: 'status'
-					},
-					{
-						data: 'action'
-					}
-				]
+				{
+					data: 'no_doc'
+				},
+				{
+					data: 'tgl_doc'
+				},
+				{
+					data: 'nmuser'
+				},
+				{
+					data: 'informasi'
+				},
+				{
+					data: 'nominal',
+					className: 'text-right'
+				},
+				{
+					data: 'status'
+				},
+				{
+					data: 'action',
+					orderable: false
+				}
+			],
+			language: {
+				processing: "Memuat data...",
+				search: "Cari:",
+				lengthMenu: "Tampilkan _MENU_ data",
+				info: "Menampilkan _START_ - _END_ dari _TOTAL_ data",
+				infoEmpty: "Tidak ada data",
+				zeroRecords: "Data tidak ditemukan",
+				paginate: {
+					first: "Pertama",
+					last: "Terakhir",
+					next: "Berikutnya",
+					previous: "Sebelumnya"
+				}
 			}
 		});
 	}
