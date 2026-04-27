ad54e78a574bddcebb93129a1f5e4d59f4104b20
Your Name <you@example.com>
2026-04-27 11:27:01 +0700

    Perbaikan - PR Department

--- application/modules/non_rutin/models/Non_rutin_model.php
+++ application/modules/non_rutin/models/Non_rutin_model.php
@@ -619,10 +619,7 @@ class Non_rutin_model extends BF_Model
             $dt_tanggal    = implode("<br>", $arr_tanggal);
             $dt_ket    = implode("<br>", $arr_ket);
 
-            $nestedData[]    = "<div align='left'>" . $dt_nama_barang . "</div>";
-            $nestedData[]    = "<div align='left'>" . $dt_spec . "</div>";
-            $nestedData[]    = "<div align='left'>" . $dt_qty . "</div>";
-            $nestedData[]    = "<div align='left'>" . $dt_ket . "</div>";
+            $nestedData[]    = "<div align='left'>" . $row['project_name'] . "</div>";
             $nestedData[]    = "<div align='left'>" . $row['nm_lengkap'] . "</div>";
             $nestedData[]    = "<div align='left'>" . $dt_tanggal . "</div>";
 
@@ -686,6 +683,9 @@ class Non_rutin_model extends BF_Model
                     }
                 }
             }
+
+            $tingkat_pr = $this->get_tingkat_pr2($row);
+            $nestedData[] = $tingkat_pr;
 
             $nestedData[]    = "<div align='left'><span class='badge' style='background-color: " . $warna . ";'>" . $sts . "</span></div>";
             $view        = "<a href='" . base_url('non_rutin/add/' . $row['no_pengajuan'] . '/view') . "' class='btn btn-sm btn-warning' title='View' data-role='qtip'><i class='fa fa-eye'></i></a>";
@@ -783,7 +783,7 @@ class Non_rutin_model extends BF_Model
             2 => 'b.nama'
         );
 
-        $sql .= " ORDER BY id DESC, " . $columns_order_by[$column_order] . " " . $column_dir . " ";
+        $sql .= " ORDER BY a.tingkat_pr DESC, id DESC, " . $columns_order_by[$column_order] . " " . $column_dir . " ";
         $sql .= " LIMIT " . $limit_start . " ," . $limit_length . " ";
 
         $data['query'] = $this->db->query($sql);
@@ -835,7 +835,7 @@ class Non_rutin_model extends BF_Model
             2 => 'b.nama'
         );
 
-        $sql .= " ORDER BY id DESC, " . $columns_order_by[$column_order] . " " . $column_dir . " ";
+        $sql .= " ORDER BY a.tingkat_pr DESC, id DESC, " . $columns_order_by[$column_order] . " " . $column_dir . " ";
         $sql .= " LIMIT " . $limit_start . " ," . $limit_length . " ";
 
         $data['query'] = $this->db->query($sql);
@@ -1068,10 +1068,7 @@ class Non_rutin_model extends BF_Model
             $dt_tanggal    = implode("<br>", $arr_tanggal);
             $dt_ket    = implode("<br>", $arr_ket);
 
-            $nestedData[]    = "<div align='left'>" . $dt_nama_barang . "</div>";
-            $nestedData[]    = "<div align='left'>" . $dt_spec . "</div>";
-            $nestedData[]    = "<div align='left'>" . $dt_qty . "</div>";
-            $nestedData[]    = "<div align='left'>" . $dt_ket . "</div>";
+            $nestedData[]    = "<div align='left'>" . $row['project_name'] . "</div>";
             $nestedData[]    = "<div align='left'>" . $row['nm_lengkap'] . "</div>";
             $nestedData[]    = "<div align='left'>" . $dt_tanggal . "</div>";
 
@@ -1107,7 +1104,13 @@ class Non_rutin_model extends BF_Model
                 }
             }
 
+            $tingkat_pr = $this->get_tingkat_pr2($row);
+
+            $nestedData[] = $tingkat_pr;
+
             $nestedData[]    = "<div align='left'><span class='badge' style='background-color: " . $warna . ";'>" . $sts . "</span></div>";
+
+
             $view        = "<a href='" . base_url('non_rutin/add/' . $row['no_pengajuan'] . '/view') . "' class='btn btn-sm btn-warning' title='View' data-role='qtip'><i class='fa fa-eye'></i></a>";
             $edit        = "";
             $approve    = "";
@@ -1118,6 +1121,8 @@ class Non_rutin_model extends BF_Model
             // $approve = '';
             // if ($ENABLE_MANAGE) {
             $approve    = "&nbsp;<a href='" . base_url('non_rutin/add_finance/' . $row['no_pengajuan'] . '/approve/3') . "' class='btn btn-sm btn-info' title='Approve' data-role='qtip'><i class='fa fa-check'></i></a>";
+
+
             // }
             $nestedData[]    = "<div align='left'>
 									" . $view . "
@@ -1126,6 +1131,8 @@ class Non_rutin_model extends BF_Model
 									" . $cancel . "
 									" . $print . "
 									</div>";
+
+
             $data[] = $nestedData;
             $urut1++;
             $urut2++;
@@ -1139,5 +1146,182 @@ class Non_rutin_model extends BF_Model
         );
 
         echo json_encode($json_data);
+    }
+
+    private function _main_query($limit = null, $offset = 0, $where = null)
+    {
+        $this->db->select('a.*, c.nm_lengkap, d.name as nm_dept, e.name as nm_company');
+        $this->db->from('rutin_non_planning_detail z');
+        $this->db->join('rutin_non_planning_header a', 'z.no_pengajuan = a.no_pengajuan', 'left');
+        $this->db->join('users c', 'c.id_user = a.created_by', 'left');
+        $this->db->join('departments d', 'd.id = a.id_dept', 'left');
+        $this->db->join('hris_companies e', 'e.id = d.company_id', 'left');
+        $this->db->where('a.status_id', 1);
+        if ($this->auth->user_id() !== '7') {
+            $this->db->where('a.created_by', $this->auth->user_id()); // penyesuaian berdasarkan department_id user
+        }
+
+        if (!empty($where)) {
+            $arr_where = $where;
+
+            $no = 1;
+            foreach ($arr_where as $key => $value) {
+                if ($no == 1) : $this->db->like($key, $value);
+                else : $this->db->or_like($key, $value);
+                endif;
+                $no++;
+            }
+        }
+
+        $this->db->where('a.close_pr', null);
+        $this->db->group_by('z.no_pengajuan');
+        $this->db->order_by('a.tingkat_pr', 'DESC');
+        // Order by status: waiting (0) → rejected (1) → approved (2)
+        $this->db->order_by('CASE WHEN (a.rejected IS NULL OR a.rejected != 1) AND a.app_3 IS NULL THEN 0 WHEN a.rejected = 1 THEN 1 ELSE 2 END', 'ASC', FALSE);
+        // Urgent (2) before Normal (1)
+        $this->db->order_by('a.no_pr IS NOT NULL', 'ASC', FALSE);
+        $this->db->order_by('a.created_date', 'DESC');
+
+        if ($limit !== null) {
+            $this->db->limit($limit, $offset);
+        }
+
+        $get_data = $this->db->get();
+
+        return $get_data;
+    }
+
+    public function get_data_non_rutin($draw, $length, $start, $search, $order, $columns)
+    {
+        $query_all = $this->_main_query();
+
+        $total_all = $query_all->num_rows();
+
+        $arr_where = [];
+        if (!empty($search['value'])) {
+            $arr_where = [
+                'a.no_pengajuan' => $search['value'],
+                'd.name' => $search['value'],
+                'e.name' => $search['value'],
+                'c.nm_lengkap' => $search['value'],
+                'a.project_name' => $search['value']
+            ];
+        }
+        $query_filter = $this->_main_query(null, null, $arr_where);
+
+        $total_filter = $query_filter->num_rows();
+
+        $query_final = $this->_main_query($length, $start, $arr_where);
+
+        $hasil = [];
+        $no = (0 + $start);
+        foreach ($query_final->result() as $item) {
+            $no++;
+            $status = $this->get_status($item);
+            $options = $this->get_option($item);
+            $tingkat_pr = $this->get_tingkat_pr($item);
+            $hasil[] = [
+                'no' => $no,
+                'no_pr' => (!empty($item->no_pr)) ? $item->no_pr : '<span class="text-red">' . $item->no_pengajuan . '</span>',
+                'departemen' => strtoupper($item->nm_dept . ' - ' . $item->nm_company),
+                'keterangan' => $item->project_name,
+                'tingkat_pr' => $tingkat_pr,
+                'pic' => $item->nm_lengkap,
+                'created_date' => date('d F Y H:i:s', strtotime($item->created_date)),
+                'status' => $status,
+                'option' => $options
+            ];
+        }
+
+        $response = [
+            'draw' => intval($draw),
+            'recordsTotal' => $total_all,
+            'recordsFiltered' => $total_filter,
+            'data' => $hasil
+        ];
+
+        echo json_encode($response);
+    }
+
+    public function get_status($data)
+    {
+        if ($data->sts_app == 'N') {
+            $warna     = 'blue';
+            $sts     = 'WAITING APPROVAL';
+        } elseif ($data->sts_app == 'Y') {
+            $warna     = 'green';
+            $sts     = 'APPROVED';
+        } else {
+            $warna     = 'red';
+            $sts     = 'REJECTED';
+        }
+
+        if (($data->sts_reject1 !== null || $data->sts_reject2 !== null || $data->sts_reject3 !== null) && $data->rejected == 1) {
+            $warna = 'red';
+            $sts = 'Rejected';
+        } else {
+            if ($data->app_3 == null) {
+                $warna = 'blue';
+                // Tentukan waiting approval di level mana
+                if (empty($data->app_1_by) && empty($data->app_2_by)) {
+                    $sts = 'Waiting Approval: Finance';
+                } else {
+                    $sts = 'Waiting Approval: Management';
+                }
+            } else {
+                if ($data->sts_app == 'Y') {
+                    $warna = "green";
+                    $sts = "Approved";
+                }
+            }
+        }
+
+        return '<span class="badge" style="background-color: ' . $warna . '">' . $sts . '</span>';
+    }
+
+    public function get_option($data)
+    {
+        $ENABLE_ADD     = has_permission('PR_Departemen.Add');
+        $ENABLE_MANAGE  = has_permission('PR_Departemen.Manage');
+        $ENABLE_VIEW    = has_permission('PR_Departemen.View');
+        $ENABLE_DELETE  = has_permission('PR_Departemen.Delete');
+
+        $view        = "<a href='" . base_url('non_rutin/add/' . $data->no_pengajuan . '/view') . "' class='btn btn-sm btn-warning' title='View' data-role='qtip'><i class='fa fa-eye'></i></a>";
+        $edit        = "";
+        $approve    = "";
+        $cancel        = "";
+        $print    = "&nbsp;<a href='" . base_url('non_rutin/print_pengajuan_non_rutin/' . $data->no_pengajuan) . "' target='_blank' class='btn btn-sm btn-success' title='Print'><i class='fa fa-print'></i></a>";
+
+        if ($data->sts_app == 'N' || $data->sts_app == '') {
+            $edit    = "&nbsp;<a href='" . base_url('non_rutin/add/' . $data->no_pengajuan) . "' class='btn btn-sm btn-primary' title='Edit' data-role='qtip'><i class='fa fa-edit'></i></a>";
+        }
+
+        $close = '';
+        if ($ENABLE_DELETE) {
+            $close = '<button type="button" class="btn btn-sm btn-danger close_pr_modal" data-no_pengajuan="' . $data->no_pengajuan . '" title="Close PR"><i class="fa fa-close"></i></button>';
+        }
+
+        $hasil = $view . ' ' . $edit . ' ' . $approve . ' ' . $cancel . ' ' . $print . ' ' . $close;
+        return $hasil;
+    }
+
+    public function get_tingkat_pr($data)
+    {
+        $tingkat_pr = '<span class="badge bg-blue">Normal</span>';
+        if ($data->tingkat_pr == '2') {
+            $tingkat_pr = '<span class="badge bg-red">Urgent</span>';
+        }
+
+        return $tingkat_pr;
+    }
+
+    public function get_tingkat_pr2($data)
+    {
+        $tingkat_pr = '<span class="badge bg-blue">Normal</span>';
+        if ($data['tingkat_pr'] == '2') {
+            $tingkat_pr = '<span class="badge bg-red">Urgent</span>';
+        }
+
+        return $tingkat_pr;
     }
 }
