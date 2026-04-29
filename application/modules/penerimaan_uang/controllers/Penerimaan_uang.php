f626db0e0b4362caf9270f1cebfab3c4a61aa24d
Your Name <you@example.com>
2026-04-29 21:39:25 +0700

    Perbaikan - Penerimaan

--- application/modules/penerimaan_uang/controllers/Penerimaan_uang.php
+++ application/modules/penerimaan_uang/controllers/Penerimaan_uang.php
@@ -154,9 +154,9 @@ class Penerimaan_uang extends Admin_Controller
             $nm_coa_bank = (!empty($get_coa_bank)) ? $get_coa_bank['nm_coa'] : '';
 
             $saldo_piutang = $get_inv['saldo_piutang'];
-            if ($pph23_dipotong == 'N') {
-                $saldo_piutang = $get_inv['saldo_piutang_tanpa_pph'];
-            }
+            // if ($pph23_dipotong == 'N') {
+            //     $saldo_piutang = $get_inv['saldo_piutang_tanpa_pph'];
+            // }
 
             $hasil .= '<tr>';
             $hasil .= '<td class="text-center">';
@@ -184,7 +184,8 @@ class Penerimaan_uang extends Admin_Controller
             $hasil .= '</td>';
             $hasil .= '</tr>';
 
-            $total_piutang += ($pph23_dipotong == 'N') ? $get_inv['tagihan_ppn_jurnal'] : $get_inv['total_akhir_jurnal'];
+            // $total_piutang += ($pph23_dipotong == 'N') ? $get_inv['tagihan_ppn_jurnal'] : $get_inv['total_akhir_jurnal'];
+            $total_piutang += $get_inv['total_akhir_jurnal'];
             $total_piutang_dagang += $saldo_piutang;
 
             if ($no == 1) {
