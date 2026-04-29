1943dc4d1e2dd43877440fefa9488dca0431d1a8
Your Name <you@example.com>
2026-04-29 15:08:14 +0700

    Perbaikan - Penerimaan Uang

--- application/modules/penerimaan_uang/views/add.php
+++ application/modules/penerimaan_uang/views/add.php
@@ -424,6 +424,22 @@
             return false;
         }
 
+        // Validasi balance jurnal: total debit harus sama dengan total kredit
+        var total_debit_jurnal = parseFloat($('input[name="total_debit_jurnal"]').val()) || 0;
+        var total_kredit_jurnal = parseFloat($('input[name="total_kredit_jurnal"]').val()) || 0;
+
+        if (total_debit_jurnal !== total_kredit_jurnal) {
+            swal({
+                type: 'error',
+                title: 'Jurnal Tidak Balance !',
+                text: 'Total Debit (' + number_format(total_debit_jurnal) + ') tidak sama dengan Total Kredit (' + number_format(total_kredit_jurnal) + '). Data tidak dapat disimpan.',
+                showConfirmButton: true,
+                allowOutsideClick: false,
+            });
+
+            return false;
+        }
+
         swal({
             type: 'warning',
             title: 'Are you sure ?',
@@ -489,5 +505,27 @@
         }
 
         return nilai;
+    }
+
+    function number_format(number, decimals, dec_point, thousands_sep) {
+        number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+        var n = !isFinite(+number) ? 0 : +number,
+            prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+            sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+            dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+            s = '',
+            toFixedFix = function(n, prec) {
+                var k = Math.pow(10, prec);
+                return '' + Math.round(n * k) / k;
+            };
+        s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+        if (s[0].length > 3) {
+            s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+        }
+        if ((s[1] || '').length < prec) {
+            s[1] = s[1] || '';
+            s[1] += new Array(prec - s[1].length + 1).join('0');
+        }
+        return s.join(dec);
     }
 </script>
