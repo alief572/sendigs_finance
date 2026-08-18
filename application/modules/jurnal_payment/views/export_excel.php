593c011f5c5e2176a2e66a4da0d9b06f18083fc9
alief572 <146901652+alief572@users.noreply.github.com>
2026-08-18 11:11:51 +0700

    Fix: Jurnal Payment
    Penambahan kolom No. Pengajuan di index nya

--- application/modules/jurnal_payment/views/export_excel.php
+++ application/modules/jurnal_payment/views/export_excel.php
@@ -1,40 +1,26 @@
 <?php
 header("Content-type: application/vnd-ms-excel");
-header("Content-Disposition: attachment; filename=Jurnal Payment.xls");
+header("Content-Disposition: attachment; filename=Jurnal_Payment_" . date('YmdHis') . ".xls");
 ?>
 <table width="100%" border="1">
     <thead>
         <tr>
-            <th class="text-center">No.</th>
-            <th class="text-center">No. Transaksi</th>
-            <th class="text-center">Jenis Transaksi</th>
-            <th class="text-center">Tanggal Jurnal</th>
-            <th class="text-center">Company</th>
+            <th class="text-center" style="background-color: #f2f2f2; font-weight: bold; text-align: center;">No.</th>
+            <th class="text-center" style="background-color: #f2f2f2; font-weight: bold; text-align: center;">No. Transaksi</th>
+            <th class="text-center" style="background-color: #f2f2f2; font-weight: bold; text-align: center;">No. Pengajuan</th>
+            <th class="text-center" style="background-color: #f2f2f2; font-weight: bold; text-align: center;">Kategori Transaksi</th>
+            <th class="text-center" style="background-color: #f2f2f2; font-weight: bold; text-align: center;">Tanggal Jurnal</th>
+            <th class="text-center" style="background-color: #f2f2f2; font-weight: bold; text-align: center;">Company</th>
         </tr>
     </thead>
     <tbody>
         <?php $no = 0;
-        foreach ($list_jurnal as $item_jurnal) : $no++;
-            $nilai = 0;
-            if ($item_jurnal['debit'] > 0) {
-                $nilai = $item_jurnal['debit'];
-            }
-            if ($item_jurnal['kredit'] > 0) {
-                $nilai = $item_jurnal['kredit'];
-            }
-
-            $status = "Open";
-            if ($item_jurnal['sts'] == '1') {
-                $status = "Posted";
-            }
-            if ($item_jurnal['sts'] == '9') {
-                $status = "Revisi";
-            }
-        ?>
+        foreach ($list_jurnal as $item_jurnal) : $no++; ?>
             <tr>
                 <td style="text-align: center;"><?= $no ?></td>
                 <td style="text-align: center;"><?= $item_jurnal['no_transaksi'] ?></td>
-                <td style="text-align: center;"><?= $item_jurnal['jenis_transaksi'] ?></td>
+                <td style="text-align: center;"><?= $item_jurnal['no_pengajuan'] ?></td>
+                <td style="text-align: center;"><?= $item_jurnal['kategori_payment'] ?></td>
                 <td style="text-align: center;"><?= date('d F Y', strtotime($item_jurnal['tgl_jurnal'])) ?></td>
                 <td style="text-align: center;"><?= $item_jurnal['nm_company'] ?></td>
             </tr>
